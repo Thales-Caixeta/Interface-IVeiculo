98f318d961f7939642d4d904545514a3962bccca
Thales-Caixeta <thalesmachado444@gmail.com>
2024-10-07 22:18:36 -0300

    main.java
    Contém o método principal (main) que cria instâncias de Automovel e Motocicleta, configurando-as (como placa e cor) e executando ações como acelerar, frear, ligar e desligar.
    Objetivo: Demonstrar como usar as classes Automovel e Motocicleta

---
+++ main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+public static void main(String[] args) {
+
+Automovel carro = new Automovel();
+
+carro.setPlaca("ABC-1234");
+
+carro.setCor("Vermelho");
+
+carro.acelerar(60);
+
+carro.frear(20);
+
+carro.ligar();
+
+carro.desligar();
+
+Motocicleta moto = new Motocicleta();
+
+moto.setPlaca("XYZ-9876");
+
+moto.acelerar(50);
+
+moto.frear(30);
+
+moto.ligar();
+
+moto.desligar();
+
+}
+
+}
